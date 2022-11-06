636821c0322aa9bffd5df5203b2bdad622a19d47
shinmyou <takuya.shinmyou@hybitz.co.jp>
2022-11-06 17:16:28 +0900

    ✨ TaskCardsコンポーネント直下のコンポーネントを作成

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { TaskCardTitle } from "./TaskCardTitle";
+import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
+import { TaskAddInput } from "./input/TaskAddInput";
+import { Tasks } from "./Tasks";
 
 export const TaskCard = () => {
-  return <div>TaskCard</div>;
+  return (
+    <div className="taskCard">
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput />
+      <Tasks />
+    </div>
+  );
 };
