7b311d0adb5962ef2856031f4baadfd7eea341da
shinmyou <takuya.shinmyou@hybitz.co.jp>
2022-11-06 16:21:38 +0900

    ✨ TaskCards

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import { Header } from "./components/Header/Header";
+import { TaskCards } from "./components/task/TaskCards";
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <TaskCards />
     </div>
   );
 }
