e8b3f3268362b3af9b29717a4c2bf9589e2878f2
shinmyou <takuya.shinmyou@hybitz.co.jp>
2022-11-06 18:22:46 +0900

    ✨ TaskAddInput

--- src/components/task/input/TaskAddInput.jsx
+++ src/components/task/input/TaskAddInput.jsx
@@ -1,5 +1,30 @@
 import React from "react";
 
-export const TaskAddInput = () => {
-  return <div>TaskAddInput</div>;
+export const TaskAddInput = ({
+  inputText,
+  setInputText,
+  taskList,
+  setTaskList,
+}) => {
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(e);
+  };
+
+  const handleChange = (e) => {
+    setInputText(e.target.value);
+    console.log(inputText);
+  };
+  return (
+    <div>
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="add a task"
+          className="taskAddInput"
+          onChange={handleChange}
+        />
+      </form>
+    </div>
+  );
 };
