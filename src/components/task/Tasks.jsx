f454d5ce768938249e6803c2b282737dc590a2be
shinmyou <takuya.shinmyou@hybitz.co.jp>
2022-11-08 23:41:42 +0900

    ✨ タスク並び替え

--- src/components/task/Tasks.jsx
+++ src/components/task/Tasks.jsx
@@ -2,16 +2,30 @@ import React from "react";
 import { DragDropContext, Droppable } from "react-beautiful-dnd";
 import { Task } from "./Task";
 
+// タスクを並び替える
+const reorder = (taskList, startIndex, endIndex) => {
+  const removeTask = taskList.splice(startIndex, 1);
+  taskList.splice(endIndex, 0, removeTask[0]);
+};
+
 export const Tasks = ({ taskList, setTaskList }) => {
+  // ドラッグ && ドロップ終了後、タスクが並び替わる
+  const onDragEnd = (result) => {
+    // タスクを並び替える
+    reorder(taskList, result.source.index, result.destination.index);
+    setTaskList(taskList);
+  };
+
   return (
     <div>
-      <DragDropContext>
+      <DragDropContext onDragEnd={onDragEnd}>
         <Droppable droppableId="droppable">
           {(provided) => (
             <div {...provided.droppableProps} ref={provided.innerRef}>
-              {taskList.map((task) => (
+              {taskList.map((task, index) => (
                 <div key={task.id}>
                   <Task
+                    index={index}
                     task={task}
                     taskList={taskList}
                     setTaskList={setTaskList}
