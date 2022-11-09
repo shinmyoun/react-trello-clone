9dcd9b1aa627876d93c06a77592c3b273a65c14d
shinmyou <takuya.shinmyou@hybitz.co.jp>
2022-11-09 23:06:48 +0900

    ✨ タスクカード追加機能

--- src/components/task/TaskCards.jsx
+++ src/components/task/TaskCards.jsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { TaskCard } from "./TaskCard";
 import { AddTaskCardButton } from "./button/AddTaskCardButton";
+import { v4 as uuid } from "uuid";
 
 export const TaskCards = () => {
+  const taskCardId = uuid();
+
+  const [taskCardsList, setTaskCardsList] = useState([
+    {
+      id: taskCardId,
+      draggableId: `item-${taskCardId}`,
+    },
+  ]);
+
   return (
     <div className="taskCardsArea">
-      <TaskCard />
-      <AddTaskCardButton />
+      {taskCardsList.map((taskCardList) => (
+        <TaskCard key={taskCardList.id} />
+      ))}
+      <AddTaskCardButton
+        taskCardsList={taskCardsList}
+        setTaskCardsList={setTaskCardsList}
+      />
     </div>
   );
 };
--- src/components/task/button/AddTaskCardButton.jsx
+++ src/components/task/button/AddTaskCardButton.jsx
@@ -1,9 +1,24 @@
 import React from "react";
+import { v4 as uuid } from "uuid";
 
-export const AddTaskCardButton = () => {
+export const AddTaskCardButton = ({ taskCardsList, setTaskCardsList }) => {
+  const addTaskCard = () => {
+    const taskCardId = uuid();
+
+    // タスクカードを追加
+    setTaskCardsList([
+      ...taskCardsList,
+      {
+        id: taskCardId,
+        draggableId: `item-${taskCardId}`,
+      },
+    ]);
+  };
   return (
     <div className="addTaskCardButtonAreas">
-      <button className="addTaskCardButton">+</button>
+      <button className="addTaskCardButton" onClick={addTaskCard}>
+        +
+      </button>
     </div>
   );
 };
